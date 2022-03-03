404a689ee2bc3afecc8ae96e5109af3ef33697f0
brian120488 <brian.yang@csedge.org>
2022-03-03 09:14:51 -0500

    implemented globals() in draw methods

--- main.py
+++ main.py
@@ -17,27 +17,12 @@ LEFT_BOUND = float(config['settings']['left_bound_scale']) * WIDTH
 RIGHT_BOUND = float(config['settings']['right_bound_scale']) * WIDTH
 TOP_BOUND = float(config['settings']['top_bound_scale']) * HEIGHT
 BOTTOM_BOUND = float(config['settings']['bottom_bound_scale']) * HEIGHT
+PATH = config.get('paths', 'sprites', fallback='sprites')
 
 pygame.init()
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Binding of Isaac')  
 clock = pygame.time.Clock()
-
-path = 'sprites'
-background = pygame.image.load(f'{path}/Monstro1.png')
-background = pygame.transform.scale(background, (WIDTH, HEIGHT))
-gameOverWill = pygame.image.load(f'{path}/last_will.png')
-gameOverWill = pygame.transform.scale(gameOverWill, (WIDTH * 0.5, HEIGHT * 0.8))
-rockImage = pygame.image.load(f'{path}/rock.png')
-
-# isGameStart = True
-# isGameOver = False
-# player = Isaac((WIDTH / 2, HEIGHT / 2))
-# enemies = [Fly((100, 100)), Maw((300, 100))]
-# rocks = []
-
-# bestTime = float(config['game']['best_time'])
-# currTime = 0
 
 # *** Global Functions ***
 def restartGame():
@@ -56,12 +41,11 @@ def restartGame():
     currTime = 0
     addRocks(rocks)
 
-def drawScore():
-    font = pygame.font.SysFont("comicsans", 16, True, False)
-    scoreText = font.render(f'Health: {player.health}', 1, (0))
-    window.blit(scoreText, (10, 10))
-
 def drawWindow():
+    global background
+    if 'background' not in globals():
+        background = pygame.image.load(f'{PATH}/background.png')
+        background = pygame.transform.scale(background, (WIDTH, HEIGHT))
     window.fill((116,63,51))
     window.blit(background, (0, 0))
     drawScore()
@@ -71,8 +55,18 @@ def drawWindow():
         enemy.draw(window)
     Projectile.drawAll(window)
     player.draw(window)
+    drawHearts(window)
     pygame.display.update()
 
+def drawScore():
+    font = pygame.font.SysFont("comicsans", 16, True, False)
+    scoreText = font.render(f'Health: {player.health}', 1, (0))
+    window.blit(scoreText, (10, 10))
+
+def drawHearts(window):
+    for i in range(player.health):
+        pass
+    
 def checkCollision(obj1, obj2):
     noXOverlap = obj1.getRight() <= obj2.getLeft() or obj1.getLeft() >= obj2.getRight()
     noYOverlap = obj1.getTop() >= obj2.getBottom()  or obj1.getBottom() <= obj2.getTop()
@@ -92,55 +86,11 @@ def buildRocks(rocks, x, y, width, height, length, image):
         py = y
         rock = Sprite((px, py), (width, height), image)
         rocks.append(rock)
-        
-def drawRocks():
-    for rock in rocks:
-        rock.draw(window)
-
-def isNextToRock(obj):
-    bottom = obj.getBottom()
-    obj.setBottom(bottom + 5)
-    collisions = checkCollisionList(obj, platforms)
-    obj.setBottom(bottom)
-    if len(collisions) > 0:
-        return True
-    return False
-
-def drawGameStart():
-    pass
-
-def drawGameOver():
-    midX = (WIDTH - gameOverWill.get_width()) / 2
-    midY = (HEIGHT - gameOverWill.get_height()) / 2
-    window.fill("white")
-    window.blit(gameOverWill, (midX, midY))
-    x, y = 355, 170
-    if len(enemies) != 0:
-        deathObject = player.diedTo.__class__((x, y))
-        deathObject.draw(window)
-    font = pygame.font.SysFont("arialblack", 16, True, False)
-    if len(enemies) == 0:
-        currTimeText = font.render(f'Time: {round(currTime / FPS, 3)}', 1, (0))
-        window.blit(currTimeText, (10, HEIGHT - 50))
-    bestTimeText = font.render(f'Best Time: {bestTime}', 1, (0))
-    window.blit(bestTimeText, (10, HEIGHT - 30))
-    pygame.display.update()
-
-def checkGameStats():
-    global bestTime
-
-    write = False
-
-    time = round(currTime / FPS, 3)
-    if time < bestTime and len(enemies) == 0:
-        config.set('game', 'best_time', str(time))
-        write = True
-        
-    if write:
-        with open('config.ini', 'w') as configfile:
-            config.write(configfile)
 
 def addRocks(rocks):
+    global rockImage
+    if 'rockImage' not in globals():
+        rockImage = pygame.image.load(f'{PATH}/rock.png') 
     width, height = 40, 40
 
     # top left corner
@@ -167,6 +117,63 @@ def addRocks(rocks):
     buildRocks(rocks, x, y, width, height, 2, rockImage)
     buildRocks(rocks, x + width, y - height, width, height, 1, rockImage)
 
+def drawRocks():
+    for rock in rocks:
+        rock.draw(window)
+
+def isNextToRock(obj):
+    bottom = obj.getBottom()
+    obj.setBottom(bottom + 5)
+    collisions = checkCollisionList(obj, platforms)
+    obj.setBottom(bottom)
+    if len(collisions) > 0:
+        return True
+    return False
+
+def drawGameStart(window):
+    global logo
+    if 'logo' not in globals():
+        logo = pygame.image.load(f'{PATH}/binding_of_isaac_logo.png').convert()
+        logo = pygame.transform.scale(logo, (WIDTH, HEIGHT * 0.8))
+    window.fill('white')
+    window.blit(logo, (0, 0))
+    pygame.display.update()
+
+def drawGameOver(window):
+    global gameOverWill
+    if 'gameOverWill' not in globals():
+        gameOverWill = pygame.image.load(f'{PATH}/last_will.png')
+        gameOverWill = pygame.transform.scale(gameOverWill, (WIDTH * 0.5, HEIGHT * 0.8))
+    midX = (WIDTH - gameOverWill.get_width()) / 2
+    midY = (HEIGHT - gameOverWill.get_height()) / 2
+    window.fill("white")
+    window.blit(gameOverWill, (midX, midY))
+    x, y = 355, 170
+    if len(enemies) != 0:
+        deathObject = player.diedTo.__class__((x, y))
+        deathObject.draw(window)
+    font = pygame.font.SysFont("arialblack", 16, True, False)
+    if len(enemies) == 0:
+        currTimeText = font.render(f'Time: {round(currTime / FPS, 3)}', 1, (0))
+        window.blit(currTimeText, (10, HEIGHT - 50))
+    bestTimeText = font.render(f'Best Time: {bestTime}', 1, (0))
+    window.blit(bestTimeText, (10, HEIGHT - 30))
+    pygame.display.update()
+
+def checkGameStats():
+    global bestTime
+
+    write = False
+
+    time = round(currTime / FPS, 2)
+    if time < bestTime and len(enemies) == 0:
+        config.set('game', 'best_time', str(time))
+        write = True
+        
+    if write:
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+
 
 # *** Pygame Loop ***
 restartGame()
@@ -180,12 +187,12 @@ while True:
     
     keys = pygame.key.get_pressed()
     if isGameStart:
-        drawGameStart()
+        drawGameStart(window)
         if keys[pygame.K_SPACE]:
             isGameStart = False
         continue
     elif isGameOver:
-        drawGameOver()
+        drawGameOver(window)
         checkGameStats()
         if keys[pygame.K_SPACE]:
             restartGame()
